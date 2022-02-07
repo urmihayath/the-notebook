c90b0467aea15ab3430b5ba85f54547f8243a4ed
Urmi Binte Hayath <90355516+urmihayath@users.noreply.github.com>
2022-02-07 11:40:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = process.env.port || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
